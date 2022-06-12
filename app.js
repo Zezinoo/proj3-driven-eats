f4f8aace43baefdf6a7fa3501897491655468e45
josevitor <josevitoralves561@gmail.com>
2022-06-12 01:19:14 -0300

    button activation added

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+let main_course
+let drink
+let dessert
+
+
 function selectMainCourse(element){
     let price = element.children[3];
     let tick = price.children[0];
-    console.log(tick)
+    main_course = element.children[1].innerHTML
  
     const clickedBox = document.querySelector('.main_course .selected')
     if (element.classList.contains('selected')){
         tick.classList.toggle('hidden')
         element.classList.remove('selected')
+        main_course = null
+        console.log("mc")
+        activateOrder()
         return null
     }
     if (clickedBox != null){
@@ -17,19 +25,21 @@ function selectMainCourse(element){
     }
     element.classList.add('selected')
     tick.classList.toggle('hidden')
-    return
+    activateOrder()
+    return element.children[1]
 }
-
 function selectDrink(element){
     let price = element.children[3];
     let tick = price.children[0];
-    console.log(element)
-    console.log(tick)
+    drink = element.children[1].innerHTML
  
     const clickedBox = document.querySelector('.drink .selected')
     if (element.classList.contains('selected')){
         tick.classList.toggle('hidden')
         element.classList.remove('selected')
+        drink = null
+        console.log("drink")
+        activateOrder()
         return null
     }
     if (clickedBox != null){
@@ -40,18 +50,21 @@ function selectDrink(element){
     }
     element.classList.add('selected')
     tick.classList.toggle('hidden')
-    return
+    activateOrder()
+    return element.children[1]
 }
 function selectDessert(element){
     let price = element.children[3];
     let tick = price.children[0];
-    console.log(element)
-    console.log(tick)
+    dessert = element.children[1].innerHTML
  
     const clickedBox = document.querySelector('.dessert .selected')
     if (element.classList.contains('selected')){
         tick.classList.toggle('hidden')
         element.classList.remove('selected')
+        dessert = null
+        console.log("dessert")
+        activateOrder()
         return null
     }
     if (clickedBox != null){
@@ -62,16 +75,22 @@ function selectDessert(element){
     }
     element.classList.add('selected')
     tick.classList.toggle('hidden')
-    return
+    activateOrder()
+    return element.children[1]
 }
-
-let main_course = selectMainCourse();
-let drink = selectDrink();
-let dessert = selectDessert();
 
 function activateOrder(){
+    let button = document.querySelector('.select_menu .button')
     if(main_course && drink && dessert){
-        let button = document.querySelector('select_menu')
-        button.classList.add('selected')
+        button.classList.remove('deactivated')
+        button.classList.add('activated')
+        button.children[0].innerHTML = 'Fechar pedido'
+    }
+    else{
+        button.classList.add('deactivated')
+        button.classList.remove('activated')
+        button.children[0].innerHTML = 'Selecione os 3 itens para fechar o pedido'
     }
 }
+
+
